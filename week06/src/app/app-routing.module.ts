d1b11b102d87f538247f8a3d208eb44f78713264
alekdjurovski <alek.djurovski@gmail.com>
2019-01-18 23:58:34 +0100

    redirect to home page

--- week06/src/app/app-routing.module.ts
+++ week06/src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { CategoriesComponent } from './components/categories-parent/categories/c
 import { CategoriesParentComponent } from './components/categories-parent/categories-parent.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/categories', pathMatch: 'full' },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
   {path: 'home', component: CategoriesParentComponent},
   {path: 'categories', component: CategoriesComponent},
   {path: 'products', component: ProductsComponent}
